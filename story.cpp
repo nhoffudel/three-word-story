d03e866fd421e2fa69c555b12580d028595a8290
nhoffudel <nhoff@udel.edu>
2015-09-10 16:53:48 +0000

    changed hello.cpp

--- story.cpp
+++ story.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include "story.h"
+using namespace std;
 
+int main(){
+    String str;
+    int add_to_story(){
+    cout<<"Enter three words: ";
+    cin>>str;
+    return 0;
+    }
