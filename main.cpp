9541a3c4ae451f3729dcd0b0632b7b59fe31c7af
nhoffudel <nhoff@udel.edu>
2015-09-10 16:58:08 +0000

    changed main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@ int main(){
     cout << "Welcome to the three word story game.  How many rounds do you want?" << endl;
     
     cin >> rounds;
-    
+    int add_to_story(String s){
+        the_story.append(s);
+    }
     for(int i = 0; i < rounds; i++){
         add_to_story(the_story);
     }
